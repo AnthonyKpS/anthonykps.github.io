5f2a8e456a38effb48f32ee9d9441400427adf86
Anthony Kaparounakis <antonikapas@gmail.com>
2020-05-05 20:32:51 +0300

    Minor updates

---
+++ main.js
@@ -0,0 +1,11 @@
+function validate() {
+
+    var name = document.contact.name.value;
+    var email = document.contact.email.value;
+    var text = document.contact.text.value;
+    var complete = false;
+
+    console.log(name);
+
+
+}
